c9474406a9744f369040ef8a4f54afd4eb5044d7
Joe Clinton <48254978+Joeclinton1@users.noreply.github.com>
2020-03-30 13:54:35 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import math
 
-steps = 500
-steps_2 = 50
-w=0.02
-b=0.01
-y=0
+steps = 500 # number of output points to be calculated
+steps_2 = 50 # number of secondary sources within the slit
+w=0.02 # wavelength
+b=0.01 # slit diameter
+D=0 #distance from slit to wall
+
 def setup():
-    global y
-    size(500,500)
+    #draw the background lines
+    global D
+    size(500,500) #for those 
     stroke(0)
     strokeWeight(2)
     background(255)
@@ -17,24 +19,28 @@ def setup():
     strokeWeight(1)
     stroke(200)
     line(width*(1-b)/2,3*height/4,width*(1+b)/2,3*height/4);
-    y=height*4
+    D=height*4 # sets distance to some arbitarily large value. 
     
 def draw():
     intensity_plot = {}
-    for i in range(steps):
+    for i in range(steps): #Loop through wall output points
         x1 = i*width/steps
-        for u in range(steps_2):
+        for u in range(steps_2): # Loop through secondary sources
             x2 = width*(1-b)/2 + u*b*width/steps_2 
-            path_dist = math.sqrt((x1-x2)**2+y**2)
-            intensity= (math.cos(path_dist/(w*width)*(TWO_PI)))
+            path_dist = math.sqrt((x1-x2)**2+D**2) #Calculate path distance from secondary source to point on wall
+            intensity= (math.cos(path_dist/(w*width)*(TWO_PI))) #Find phase of wavelength with path distance and map from -1 to 1
+            #sum intensity to the intensity plot
             if u == 0:
                 intensity_plot[i] = intensity
             else:
                 intensity_plot[i] += intensity
+    #draw the intensity plot
     for i in range(steps):
         x1 = i*width/steps
         stroke((intensity_plot[i]**2)/10)
         line(x1,height/4,x1,height/5);
+        
+#change wavelength with up or down arrows
 def keyPressed():
     global w
     if(keyCode == UP):
