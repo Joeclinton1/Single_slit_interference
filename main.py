d6edfe559084ea07959fd63076e3a33ce8bea61e
Joe Clinton <48254978+Joeclinton1@users.noreply.github.com>
2020-03-30 14:51:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import math
 
-out_pt_count = 500 # number of output points to be calculated
-sec_source_count = 50 # number of secondary sources within the slit
-w=0.02 # wavelength/width
-b=0.01 # slit diameter/width
-D=2000 #distance from slit to wall
+out_cnt = 500 # number of output points to be calculated
+src_cnt = 50 # number of secondary sources within the slit
+w=0.05 # wavelength 
+b=0.01 # slit diameter 
+D=200 #distance from slit to wall 
 
 def setup():
     #draw the background lines
@@ -20,24 +20,31 @@ def setup():
     line(width*(1-b)/2,3*height/4,width*(1+b)/2,3*height/4);
     
 def draw():
-    intensity_plot = {}
-    for i in range(out_pt_count): #Loop through wall output points
-        x1 = i*width/out_pt_count
-        for u in range(sec_source_count): # Loop through secondary sources
-            x2 = width*(1-b)/2 + u*b*width/sec_source_count
-            path_dist = math.sqrt((x1-x2)**2+D**2) #Calculate path distance from secondary source to point on wall
-            intensity= (math.cos(path_dist/(w*width)*(TWO_PI))) #Find phase of wavelength with path distance and map from -1 to 1
-            #sum intensity to the intensity plot
-            if u == 0:
-                intensity_plot[i] = intensity
-            else:
-                intensity_plot[i] += intensity
+    intensity_plt = calculate_intensity_plot()
     #draw the intensity plot
-    for i in range(out_pt_count):
-        x1 = i*width/out_pt_count
-        stroke((intensity_plot[i]**2)/10)
-        line(x1,height/4,x1,height/5);
+    for o in range(-out_cnt/2, out_cnt/2):
+        x_o = o*width/out_cnt
+        stroke((intensity_plt[o]**2)/1000)
+        line(width * (x_o + 1) /2,height/4,width * (x_o + 1) /2,height/5);
         
+def calculate_intensity_plot():
+    intensity_plt = {}
+    
+    for o in range(-out_cnt/2, out_cnt/2): #Loop through wall output points
+        x_o = o*width/out_cnt #x position on wall
+        
+        for s in range(-src_cnt/2,src_cnt/2,1): # Loop through secondary sources
+            x_s= s*b/src_cnt #x position on source
+            path_dist = math.sqrt((x_o-x_s)**2+D**2) #Calculate path distance from secondary source to point on wall
+            intensity= math.cos(path_dist/w*TWO_PI) #Find phase of wavelength with path distance and map from -1 to 1
+            
+            #sum intensity to the intensity plot
+            if s == -src_cnt/2:
+                intensity_plt[o] = intensity
+            else:
+                intensity_plt[o] += intensity
+    return intensity_plt
+                
 #change wavelength with up or down arrows
 def keyPressed():
     global w
